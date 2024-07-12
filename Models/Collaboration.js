324adbdbf19a53ffb7a2623bdbad02e1f9c479ba
Josiane <josiane.bureau17@gmail.com>
2024-07-12 10:03:43 +0200

    Créationt du modèle Collaboration

---
+++ Models/Collaboration.js
@@ -0,0 +1,46 @@
+const {Model, DataTypes} = require("sequelize");
+const sequelize = require('../Config/Sequelize');
+const User = require('./User');
+const Task = require('./Task');
+class Collaboration extends Model {
+
+}
+
+Collaboration.init({
+    US_ID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        foreignKey: true,
+        references: {
+            model: 'User',
+            key: 'US_ID'
+            },
+        allowNull: false
+        }
+    TA_ID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        foreignKey: true,
+        references: {
+            model: 'Task',
+            key: 'TA_ID'
+            },
+        allowNull: false
+        }
+
+    
+},
+    {sequelize,
+        modelName: 'Collaboration',
+        tableName: 'to_collaborate',
+        timestamps: false
+    
+})
+
+Task.hasMany(Collaboration, {as : 'tasks', foreignKey: 'TA_ID'});
+Collaboration.belongsTo(User, {as : 'User', foreignKey : 'US_ID'});
+
+module.exports = Collaboration;
+
